480970d1b62afc9aa6800120d7b8a9e810cfff63
Pothin <pothin.neza2023@kepler.org>
2026-06-01 14:16:40 +0200

    Made changes on RegisterPayload

--- src/test/java/com/thinkgetit/backend/pojo/RegisterPayload.java
+++ src/test/java/com/thinkgetit/backend/pojo/RegisterPayload.java
@@ -7,14 +7,12 @@ public class RegisterPayload {
     @JsonProperty("firstName")
     private String firstName;
 
-    // Add the missing field matching the database expectation
     @JsonProperty("lastName")
     private String lastName;
 
     private String email;
     private String password;
 
-    // Update the constructor to take all 4 fields
     public RegisterPayload(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
